6afb0d082f36759c07bb7c45b0382cae7aa60e6b
Svetlin Simonyan <svetlin.s@gmail.com>
2020-10-13 09:01:59 +0300

    Remove wrong RDoc comment

--- lib/naive_calculator/calculator.rb
+++ lib/naive_calculator/calculator.rb
@@ -1,11 +1,6 @@
 Treetop.load File.expand_path('arithmetic', __dir__)
 
 module NaiveCalculator
-  # Runs a subprocess and applies handlers for stdout and stderr
-  # Params:
-  # +command+:: command line string to be executed by the system
-  # +outhandler+:: +Proc+ object that takes a pipe object as first and only param (may be nil)
-  # +errhandler+:: +Proc+ object that takes a pipe object as first and only param (may be nil)
   class Calculator
     def initialize
       @environment = Environment.new
